196ff7f612083b704fe577b490754630da45772f
Perress <guigalinares@gmail.com>
2023-05-15 21:46:06 -0300

    update 15/05/2023 rev 9

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,20 @@
 /* Arquivo das automações do menu hamburguer */
+
+const btnMobile = document.getElementById('btn-mobile');
+//Declarando a variavel com o CONST
+
+function toggleMenu(event) {
+    if(event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('area-expanded', active);
+        if (active) {
+            event.currentTarget.setAttribute('aria-label', 'Fechar menu');
+        } else {
+            event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+        }
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
